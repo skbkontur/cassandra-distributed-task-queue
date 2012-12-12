eeee00689c1384f4f126d4e9cb7d97fbcc4b45af
mr146 <mv146@skbkontur.ru>
2012-12-12 09:40:07 +0000

    батнички, вроде все работает
    git-svn-id: svn://svn.kontur/Catalogue/trunk@144940 6dce57de-9019-ad4d-9412-f72b55faeb66

--- startAll.bat
+++ startAll.bat
@@ -1,5 +1,3 @@
-rem C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe RemoteTaskQueue.sln /target:Clean;Rebuild /verbosity:m /p:zip=false	
-
 cd ..\Assemblies\Cassandra\Server\bin\
 start cassandra.bat
 
