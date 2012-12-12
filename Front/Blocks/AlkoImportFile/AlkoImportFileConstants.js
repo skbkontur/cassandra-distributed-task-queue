aee1be6836d0b0b1cfb76d197feb4abdc3c6ea45
mr146 <mv146@skbkontur.ru>
2012-12-12 09:35:34 +0000

    еще докоммит
    git-svn-id: svn://svn.kontur/Catalogue/trunk@144935 6dce57de-9019-ad4d-9412-f72b55faeb66

--- Front/Blocks/AlkoImportFile/AlkoImportFileConstants.js
+++
@@ -1,7 +0,0 @@
-﻿var AlkoImportFileConstants = {
-    importClass: "alkoImportFile",
-    inputClass: "alkoImportFile__input",
-    chooseFileId: "ChooseFileButton",
-    chooseAnotherFileId: "ChooseAnotherFileButton",
-    formSelector: ".alkoImportFile__content__form form"
-};
