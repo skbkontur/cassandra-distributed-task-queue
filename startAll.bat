9af6aec4fe8ebbb352392c62651d6f539532a8c5
mr146 <mv146@skbkontur.ru>
2012-12-13 09:37:32 +0000

    поменял порты
    git-svn-id: svn://svn.kontur/Catalogue/trunk@145050 6dce57de-9019-ad4d-9412-f72b55faeb66

--- startAll.bat
+++ startAll.bat
@@ -13,7 +13,8 @@ if exist ExchangeService\bin\Debug\Catalogue.RemoteTaskQueue.ExchangeService.exe
 ping 123.45.67.89 -n 1 -w 1000 > nul
 if exist ExchangeService\bin\Debug\Catalogue.RemoteTaskQueue.ExchangeService.exe (start cmd /c ExchangeService\bin\Debug\Catalogue.RemoteTaskQueue.ExchangeService.exe)
 
-start cmd /c MonitoringService\bin\Debug\Catalogue.RemoteTaskQueue.MonitoringService.exe
 start cmd /c IndexService\bin\Debug\Catalogue.RemoteTaskQueue.IndexService.exe
+
+start cmd /c MonitoringService\bin\Debug\Catalogue.RemoteTaskQueue.MonitoringService.exe
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe DeployFronts.xml
