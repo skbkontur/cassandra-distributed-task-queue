9ce044b33345265d486be43a37752256af8fa481
denis.boginich <denis.boginich@skbkontur.ru>
2012-12-18 11:02:51 +0000

    Валидации на DateAndTime (Ticks, MinimalStartTicks, StartExecutedTicks)
    git-svn-id: svn://svn.kontur/Catalogue/trunk@145563 6dce57de-9019-ad4d-9412-f72b55faeb66

--- TaskMonitoringViewer/Models/RemoteTaskQueueModel.cs
+++ TaskMonitoringViewer/Models/RemoteTaskQueueModel.cs
@@ -9,12 +9,12 @@ using SKBKontur.Catalogue.Core.Web.Models.ModelConfigurations;
 
 namespace SKBKontur.Catalogue.RemoteTaskQueue.TaskMonitoringViewer.Models
 {
-    public class RemoteTaskQueueModel : PageModelBase<RemoteTaskQueueModelData>, IPaginatorModelData
+    public class RemoteTaskQueueModel : PageModelBase<RemoteTaskQueuePageModel>, IPaginatorModelData
     {
-        public RemoteTaskQueueModel(PageModelBaseParameters parameters, RemoteTaskQueueModelData remoteTaskQueueModelData)
+        public RemoteTaskQueueModel(PageModelBaseParameters parameters, RemoteTaskQueuePageModel remoteTaskQueuePageModel)
             : base(parameters)
         {
-            Data = remoteTaskQueueModelData;
+            Data = remoteTaskQueuePageModel;
         }
 
         public string GetUrl(UrlHelper url, int page)
@@ -24,17 +24,27 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.TaskMonitoringViewer.Models
 // ReSharper restore Asp.NotResolved
         }
 
-        protected override void Configure(MutatorsConfigurator<RemoteTaskQueueModelData> configurator)
+        protected override void Configure(MutatorsConfigurator<RemoteTaskQueuePageModel> configurator)
         {
             base.Configure(configurator);
+            configurator.Target(data => data.SearchPanel.Ticks.From.Date).Date();
+            configurator.Target(data => data.SearchPanel.Ticks.From.Time).Time();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.From.Date).Date();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.From.Time).Time();
             configurator.Target(data => data.SearchPanel.MinimalStartTicks.From.Date).Date();
             configurator.Target(data => data.SearchPanel.MinimalStartTicks.From.Time).Time();
+            configurator.Target(data => data.SearchPanel.Ticks.To.Date).Date();
+            configurator.Target(data => data.SearchPanel.Ticks.To.Time).Time();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.To.Date).Date();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.To.Time).Time();
+            configurator.Target(data => data.SearchPanel.MinimalStartTicks.To.Date).Date();
+            configurator.Target(data => data.SearchPanel.MinimalStartTicks.To.Time).Time();
         }
 
         public int TotalPagesCount { get; set; }
         public int PageNumber { get; set; }
         public int PagesWindowSize { get; set; }
-        public override sealed RemoteTaskQueueModelData Data { get; protected set; }
+        public override sealed RemoteTaskQueuePageModel Data { get; protected set; }
         public string SearchRequestId { get; set; }
 
         public SearchPanelHtmlModel HtmlModel { get; set; }
