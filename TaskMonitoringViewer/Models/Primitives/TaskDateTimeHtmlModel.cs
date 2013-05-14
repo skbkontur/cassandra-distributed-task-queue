465711c74d667370f04ec60121ecbc3dd1aa1249
shmakov <shmakov@skbkontur.ru>
2013-05-14 05:50:19 +0000

    Trunk merge
    git-svn-id: svn://svn.kontur/Catalogue/JsSpeedRevolution@156626 6dce57de-9019-ad4d-9412-f72b55faeb66

--- TaskMonitoringViewer/Models/Primitives/TaskDateTimeHtmlModel.cs
+++ TaskMonitoringViewer/Models/Primitives/TaskDateTimeHtmlModel.cs
@@ -1,10 +1,18 @@
-﻿using SKBKontur.Catalogue.Core.Web.Models.HtmlModels;
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+using SKBKontur.Catalogue.Core.Web.Models.HtmlModels;
 
 namespace SKBKontur.Catalogue.RemoteTaskQueue.TaskMonitoringViewer.Models.Primitives
 {
-    public class TaskDateTimeHtmlModel : IHtmlModel
+    public class TaskDateTimeHtmlModel : IRenderHtmlModel
     {
         public string Id { get; set; }
+        public MvcHtmlString ToHtmlString(HtmlHelper htmlHelper)
+        {
+            return htmlHelper.Partial("~/TaskMonitoringViewer/_TaskDateTime.cshtml", this);
+        }
+
         public string DateTime { get; set; }
         public long? Ticks { get; set; }
         public bool HideTicks { get; set; }
