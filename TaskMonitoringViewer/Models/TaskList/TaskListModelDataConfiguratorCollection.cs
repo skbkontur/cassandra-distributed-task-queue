9e1605103f7c25b1a990cb3c637684402d4c7b2e
ichevdar <ichevdar@skbkontur.ru>
2013-11-15 16:02:42 +0600

    Добавил забытые файлы

---
+++ TaskMonitoringViewer/Models/TaskList/TaskListModelDataConfiguratorCollection.cs
@@ -0,0 +1,26 @@
+﻿using GroboContainer.Core;
+
+using SKBKontur.Catalogue.Core.Web.Models.ModelConfigurations;
+using SKBKontur.Catalogue.Core.Web.PageModels;
+using SKBKontur.Catalogue.Mutators;
+
+namespace SKBKontur.Catalogue.RemoteTaskQueue.TaskMonitoringViewer.Models.TaskList
+{
+    public class TaskListModelDataConfiguratorCollection : WebDataConfiguratorCollection<TaskListModelData>
+    {
+        public TaskListModelDataConfiguratorCollection(IContainer container)
+            : base(container)
+        {
+        }
+
+        protected override void Configure(PageModelContext context, MutatorsConfigurator<TaskListModelData> configurator)
+        {
+            configurator.Target(data => data.SearchPanel.Ticks.From.Date).Date();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.From.Date).Date();
+            configurator.Target(data => data.SearchPanel.MinimalStartTicks.From.Date).Date();
+            configurator.Target(data => data.SearchPanel.Ticks.To.Date).Date();
+            configurator.Target(data => data.SearchPanel.StartExecutedTicks.To.Date).Date();
+            configurator.Target(data => data.SearchPanel.MinimalStartTicks.To.Date).Date();
+        }
+    }
+}
