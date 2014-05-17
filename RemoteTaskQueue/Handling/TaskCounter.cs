82a7035fbee93c308152874f477d5f60fad14784
tihonove <tihonove@skbkontur.ru>
2014-05-17 11:24:02 +0600

    Реализовали логику TaskCounter'а, которая не позволит очереди нажрать много тасок, если MaxContinuationTaskCount = 0 (cherry picked from commit 5f3b930f40455f83637ea62ac5dcc4c771066f2a)

--- RemoteTaskQueue/Handling/TaskCounter.cs
+++ RemoteTaskQueue/Handling/TaskCounter.cs
@@ -8,35 +8,83 @@ namespace RemoteQueue.Handling
     {
         public TaskCounter(IExchangeSchedulableRunnerSettings settings)
         {
-            pullFromQueueCounter = new SingleTaskCounter(settings.MaxRunningTasksCount);
-            continuationCounter = new SingleTaskCounter(settings.MaxRunningContinuationsCount);
+            maxRunningTasksCount = settings.MaxRunningTasksCount;
+            maxRunningContinuationsCount = settings.MaxRunningContinuationsCount;
         }
 
         public bool CanQueueTask(TaskQueueReason reason)
         {
-            return GetCounter(reason).CanQueueTask();
+            if(reason == TaskQueueReason.TaskContinuation)
+            {
+                if(maxRunningContinuationsCount == 0)
+                    return true;
+                return continuationsCount < maxRunningContinuationsCount;
+            }
+            if(reason == TaskQueueReason.PullFromQueue)
+            {
+                if(maxRunningTasksCount == 0)
+                    return true;
+                if(maxRunningContinuationsCount == 0)
+                    return (continuationsCount + tasksCount) < (maxRunningTasksCount);
+                else
+                    return (tasksCount) < (maxRunningTasksCount);
+            }
+            throw new InvalidOperationException(string.Format("Неизвестный тип TaskQueueReason: {0}", reason));
         }
 
         public bool TryIncrement(TaskQueueReason reason)
         {
-            return GetCounter(reason).TryIncrement();
+            if(reason == TaskQueueReason.TaskContinuation)
+            {
+                lock(lockObject)
+                {
+                    if(CanQueueTask(reason))
+                    {
+                        continuationsCount++;
+                        return true;
+                    }
+                    return maxRunningContinuationsCount == 0;
+                }
+            }
+            else if(reason == TaskQueueReason.PullFromQueue)
+            {
+                lock(lockObject)
+                {
+                    if(CanQueueTask(reason))
+                    {
+                        tasksCount++;
+                        return true;
+                    }
+                    return maxRunningTasksCount == 0;
+                }
+            }
+            throw new InvalidOperationException(string.Format("Неизвестный тип TaskQueueReason: {0}", reason));
         }
 
         public void Decrement(TaskQueueReason reason)
         {
-            GetCounter(reason).Decrement();
-        }
-
-        private SingleTaskCounter GetCounter(TaskQueueReason reason)
-        {
-            if(reason == TaskQueueReason.PullFromQueue)
-                return pullFromQueueCounter;
             if(reason == TaskQueueReason.TaskContinuation)
-                return continuationCounter;
-            throw new NotSupportedException(string.Format("Unknown reason of task queueing: {0}", reason));
+            {
+                lock(lockObject)
+                {
+                    continuationsCount--;
+                }
+            }
+            else if(reason == TaskQueueReason.PullFromQueue)
+            {
+                lock(lockObject)
+                {
+                    tasksCount--;
+                }
+            }
+            else
+                throw new InvalidOperationException(string.Format("Неизвестный тип TaskQueueReason: {0}", reason));
         }
 
-        private readonly SingleTaskCounter pullFromQueueCounter;
-        private readonly SingleTaskCounter continuationCounter;
+        private readonly int maxRunningTasksCount;
+        private readonly int maxRunningContinuationsCount;
+        private int tasksCount;
+        private int continuationsCount;
+        private readonly object lockObject = new object();
     }
 }
