2252d2eab10e6d31ae0258a34dd32680f6463a93
Denis Dublennykh <stigius@kontur.ru>
2014-08-28 17:31:21 +0600

    Тесты на индекс по дочерним таскам

---
+++ FunctionalTests/ExchangeTests/ChildIndexTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+using RemoteQueue.Handling;
+
+using SKBKontur.Catalogue.RemoteTaskQueue.TaskDatas;
+
+namespace FunctionalTests.ExchangeTests
+{
+    public class ChildIndexTest : FunctionalTestBase
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+            taskQueue = Container.Get<IRemoteTaskQueue>();
+        }
+
+        [Test]
+        public void NoChildrenTasksTest()
+        {
+            var taskId = taskQueue.CreateTask(new SimpleTaskData()).Queue();
+            CollectionAssert.IsEmpty(taskQueue.GetChildrenTaskIds(taskId));
+        }
+
+        [Test]
+        public void MultipleChildrenTest()
+        {
+            var taskId = taskQueue.CreateTask(new SimpleTaskData()).Queue();
+            var childTaskId1 = taskQueue.CreateTask(new SimpleTaskData(), new CreateTaskOptions {ParentTaskId = taskId}).Queue();
+            var childTaskId2 = taskQueue.CreateTask(new SimpleTaskData(), new CreateTaskOptions {ParentTaskId = taskId}).Queue();
+            var childTaskId3 = taskQueue.CreateTask(new SimpleTaskData(), new CreateTaskOptions {ParentTaskId = taskId}).Queue();
+            CollectionAssert.AreEquivalent(new[] {childTaskId1, childTaskId2, childTaskId3}, taskQueue.GetChildrenTaskIds(taskId));
+        }
+
+        [Test]
+        public void ChainTest()
+        {
+            var taskId = taskQueue.CreateTask(new SimpleTaskData()).Queue();
+            var childTaskId = taskQueue.CreateTask(new SimpleTaskData(), new CreateTaskOptions {ParentTaskId = taskId}).Queue();
+            var grandChildTaskId = taskQueue.CreateTask(new SimpleTaskData(), new CreateTaskOptions {ParentTaskId = childTaskId}).Queue();
+            CollectionAssert.AreEqual(new[] {childTaskId}, taskQueue.GetChildrenTaskIds(taskId));
+            CollectionAssert.AreEqual(new[] {grandChildTaskId}, taskQueue.GetChildrenTaskIds(childTaskId));
+        }
+
+        private IRemoteTaskQueue taskQueue;
+    }
+}
