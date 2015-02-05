e54f37fef4cbff93c609dce57d9175648cd8505e
dgorlov <dgorlov@skbkontur.ru>
2015-02-05 16:10:49 +0500

    RemoteTaskQueue: Max snapshot size increased

--- MonitoringServiceCore/Implementation/Counters/SnapshotsManager.cs
+++ MonitoringServiceCore/Implementation/Counters/SnapshotsManager.cs
@@ -1,8 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
-
-using GroBuf;
 
 using log4net;
 
@@ -14,45 +14,36 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
 {
     public class SnapshotsManager
     {
-        public SnapshotsManager(MetaProvider metaProvider, IProcessedTasksCounter processedTasksCounter, ISqlDatabase sqlDatabase, ISerializer serializer)
+        public SnapshotsManager(MetaProvider metaProvider, IProcessedTasksCounter processedTasksCounter, ISqlDatabase sqlDatabase, SnapshotConverter snapshotConverter)
         {
             this.metaProvider = metaProvider;
             this.processedTasksCounter = processedTasksCounter;
             this.sqlDatabase = sqlDatabase;
-            this.serializer = serializer;
+            this.snapshotConverter = snapshotConverter;
         }
 
         public void Init()
         {
-            sqlDatabase.CreateTableWithIndexes(tableName, new[]
-                {
-                    new SqlColumn()
-                        {
-                            DataType = SqlDataType.LongBlob, Name = hashColumn,
-                        },
-                    new SqlColumn()
-                        {
-                            DataType = SqlDataType.LongBlob, Name = dataColumn,
-                        },
-                });
+            ActualizeDatabaseScheme();
         }
 
         public void LoadSnapshot()
         {
             logger.InfoFormat("LoadSnapshot begin");
-            var bytes = ReadOrNull();
+            int version;
+            var bytes = ReadOrNull(out version);
             InternalSnapshot internalSnapshot;
             var isBad = false;
             if(bytes == null)
             {
                 logger.WarnFormat("No snapshot found");
-                internalSnapshot = emptyOldSnapshot;
+                internalSnapshot = snapshotConverter.emptyOldSnapshot;
             }
             else
             {
-                internalSnapshot = serializer.Deserialize<InternalSnapshot>(bytes);
+                internalSnapshot = snapshotConverter.ConvertFromBytes(version, bytes);
                 isBad = IsSnapshotBad(internalSnapshot);
-                logger.InfoFormat("Snapshot found. version={0}. isBad={1}", internalSnapshot.Version, isBad);
+                logger.InfoFormat("Snapshot check: isBad={0}", isBad);
             }
             if(!isBad)
                 LoadSnapshots(internalSnapshot);
@@ -66,17 +57,51 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
             var counterSnapshot = processedTasksCounter.GetSnapshotOrNull(CounterSettings.MaxStoredSnapshotLength);
             if(metaProviderSnapshot == null)
             {
-                logger.WarnFormat("MetaProvider snapshot is BIG - do nothing");
+                logger.WarnFormat("SaveSnapshot: MetaProvider snapshot is BIG - do nothing");
                 return;
             }
             if(counterSnapshot == null)
             {
-                logger.WarnFormat("ProcessedTasksCounter snapshot is BIG - do nothing");
+                logger.WarnFormat("SaveSnapshot: ProcessedTasksCounter snapshot is BIG - do nothing");
                 return;
             }
-            var internalSnapshot = new InternalSnapshot(version, metaProviderSnapshot, counterSnapshot);
-            SaveBytes(serializer.Serialize(internalSnapshot));
-            logger.InfoFormat("SaveSnapshot time={0} ok", DateTimeFormatter.FormatWithMsAndTicks(metaProviderSnapshot.LastUpdateTicks));
+            var internalSnapshot = new InternalSnapshot(metaProviderSnapshot, counterSnapshot);
+
+            SaveBytes(snapshotConverter.ConvertToBytes(internalSnapshot));
+
+            logger.InfoFormat("SaveSnapshot: NotFinishedTasks={1} NotReadEvents={2} ReadEvents={3} time={0} done",
+                              DateTimeFormatter.FormatWithMsAndTicks(metaProviderSnapshot.LastUpdateTicks),
+                              SafeCollectionLength(counterSnapshot.NotFinishedTasks),
+                              SafeDictionaryCount(metaProviderSnapshot.NotReadEvents),
+                              SafeDictionaryCount(metaProviderSnapshot.ReadEvents));
+        }
+
+        private void ActualizeDatabaseScheme()
+        {
+            sqlDatabase.CreateTableWithIndexes(tableName, columnDefinitions);
+            var columns = sqlDatabase.GetColumns(tableName);
+            foreach(var columnDefinition in columnDefinitions)
+            {
+                if(!columns.Contains(columnDefinition.Name))
+                {
+                    logger.InfoFormat("Init: add missing column '{0}'", columnDefinition.Name);
+                    sqlDatabase.AddColumns(tableName, new[] {columnDefinition});
+                }
+            }
+        }
+
+        private static int SafeDictionaryCount(IDictionary dictionary)
+        {
+            if(dictionary == null)
+                return 0;
+            return dictionary.Count;
+        }
+
+        private static int SafeCollectionLength<T>(ICollection<T> arr)
+        {
+            if(arr == null)
+                return 0;
+            return arr.Count;
         }
 
         private void LoadSnapshots(InternalSnapshot internalSnapshot)
@@ -95,25 +120,27 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
 
         private static bool IsSnapshotBad(InternalSnapshot internalSnapshot)
         {
-            return internalSnapshot.Version != version || internalSnapshot.CounterSnapshot == null || internalSnapshot.MetaProviderSnapshot == null;
+            return internalSnapshot.CounterSnapshot == null || internalSnapshot.MetaProviderSnapshot == null;
         }
 
-        private byte[] ReadOrNull()
+        private byte[] ReadOrNull(out int version)
         {
+            version = 0;
             var selectQuery = new SqlSelectQuery
                 {
                     TableName = tableName,
                     Criterion = ExpressionTree.Constant(true),
-                    NeedColumns = new[] {hashColumn, dataColumn},
+                    NeedColumns = new[] {hashColumn, dataColumn, versionColumn},
                 };
             var rows = sqlDatabase.GetRows(selectQuery);
 
-            if(rows.Count <= 0 || rows[0].Length < 2)
+            if(rows.Count <= 0 || rows[0].Length < 3)
                 return null;
             if(rows.Count > 1)
                 logger.WarnFormat("Trash in table - many rows.");
             var hash = ExtractBytes(rows[0][0]);
             var data = ExtractBytes(rows[0][1]);
+            version = ExtractInt(rows[0][2]);
             var actualHash = GetHash(data);
             var readHash = Convert.ToBase64String(hash);
             var actualHashStr = Convert.ToBase64String(actualHash);
@@ -123,6 +150,13 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
                 return null;
             }
             return data;
+        }
+
+        private static int ExtractInt(object o)
+        {
+            if (ReferenceEquals(o, DBNull.Value) || o == null)
+                return 0;
+            return (int)o;
         }
 
         private static byte[] ExtractBytes(object o)
@@ -139,6 +173,7 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
                     new[]
                         {
                             new KeyValuePair<string, object>(SqlConstants.SqlIdColumnName, "1"),
+                            new KeyValuePair<string, object>(versionColumn, SnapshotConverter.CurrentVersion),
                             new KeyValuePair<string, object>(hashColumn, GetHash(data)),
                             new KeyValuePair<string, object>(dataColumn, data),
                         }
@@ -150,32 +185,33 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementati
             return new MD5CryptoServiceProvider().ComputeHash(data);
         }
 
+        private readonly SqlColumn[] columnDefinitions = new SqlColumn[]
+            {
+                new SqlColumn
+                    {
+                        DataType = SqlDataType.LongBlob, Name = hashColumn,
+                    },
+                new SqlColumn
+                    {
+                        DataType = SqlDataType.LongBlob, Name = dataColumn,
+                    },
+                new SqlColumn()
+                    {
+                        DataType = SqlDataType.Integer,
+                        Name = versionColumn
+                    },
+            };
+
         private readonly MetaProvider metaProvider;
         private readonly IProcessedTasksCounter processedTasksCounter;
         private readonly ISqlDatabase sqlDatabase;
-        private readonly ISerializer serializer;
+        private readonly SnapshotConverter snapshotConverter;
 
-        private readonly ILog logger = LogManager.GetLogger("SnapshotsManager");
-        private readonly InternalSnapshot emptyOldSnapshot = new InternalSnapshot(version, new MetaProvider.MetaProviderSnapshot(0, 0, null, null), new ProcessedTasksCounter.CounterSnapshot(null, 0, 0));
-
-        private class InternalSnapshot
-        {
-            public InternalSnapshot(int version, MetaProvider.MetaProviderSnapshot metaProviderSnapshot, ProcessedTasksCounter.CounterSnapshot counterSnapshot)
-            {
-                Version = version;
-                MetaProviderSnapshot = metaProviderSnapshot;
-                CounterSnapshot = counterSnapshot;
-            }
-
-            public int Version { get; set; }
-            public MetaProvider.MetaProviderSnapshot MetaProviderSnapshot { get; set; }
-            public ProcessedTasksCounter.CounterSnapshot CounterSnapshot { get; set; }
-        }
+        private static readonly ILog logger = LogManager.GetLogger("SnapshotsManager");
 
         private const string dataColumn = "Data";
         private const string hashColumn = "Hash";
+        private const string versionColumn = "Version";
         private const string tableName = "Snapshots";
-
-        private const int version = 1;
     }
 }
---
+++ Tests/MonitoringServiceCore/SnapshotConverterTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.RemoteTaskQueue.MonitoringServiceCore.Implementation.Counters;
+using SKBKontur.Catalogue.TestCore;
+
+namespace RemoteQueue.Tests.MonitoringServiceCore
+{
+    public class SnapshotConverterTest : CoreTestBase
+    {
+        [Test]
+        public void TestCurrentVersion()
+        {
+            var internalSnapshot = new InternalSnapshot(new MetaProvider.MetaProviderSnapshot(128923, 238923, new Dictionary<string, long>() {{"z", 1}}, new Dictionary<string, long>() {{"q", 2}}
+                                                            ), new ProcessedTasksCounter.CounterSnapshot(new HashSet<string>() {{"x"}, {"y"}}, 3478, 38));
+
+            var bytes = snapshotConverter.ConvertToBytes(internalSnapshot);
+            Console.WriteLine("grobuf: {0} length: {1}", serializer.GetSize(internalSnapshot), bytes.Length);
+            //note content is gzipped
+            Assert.That(bytes.Length < serializer.GetSize(internalSnapshot));
+
+            var convertFromBytes = snapshotConverter.ConvertFromBytes(SnapshotConverter.CurrentVersion, bytes);
+            convertFromBytes.AssertEqualsToWithXmlSerializer(internalSnapshot);
+        }
+
+        [Test]
+        public void TestV1()
+        {
+            const int version = 1;
+            var internalSnapshot = new InternalSnapshot(new MetaProvider.MetaProviderSnapshot(128923, 238923, new Dictionary<string, long>() {{"z", 22}}, new Dictionary<string, long>() {{"q", 2}}
+                                                            ), new ProcessedTasksCounter.CounterSnapshot(new HashSet<string>() {{"x"}, {"y"}}, 3478, 38));
+            var v1 = new InternalSnapshotV1() {Version = version, CounterSnapshot = internalSnapshot.CounterSnapshot, MetaProviderSnapshot = internalSnapshot.MetaProviderSnapshot};
+            var bytes = serializer.Serialize(v1);
+
+            var convertFromBytes = snapshotConverter.ConvertFromBytes(version, bytes);
+            convertFromBytes.AssertEqualsToWithXmlSerializer(internalSnapshot);
+        }
+
+        [Test]
+        public void TestV1Zero()
+        {
+            const int version = 1;
+            var internalSnapshot = new InternalSnapshot(new MetaProvider.MetaProviderSnapshot(128923, 238923, new Dictionary<string, long>() {{"z", 22}}, new Dictionary<string, long>() {{"q", 2}}
+                                                            ), new ProcessedTasksCounter.CounterSnapshot(new HashSet<string>() {{"x"}, {"y"}}, 3478, 38));
+            var v1 = new InternalSnapshotV1() {Version = version, CounterSnapshot = internalSnapshot.CounterSnapshot, MetaProviderSnapshot = internalSnapshot.MetaProviderSnapshot};
+            var bytes = serializer.Serialize(v1);
+
+            var convertFromBytes = snapshotConverter.ConvertFromBytes(0, bytes);
+            convertFromBytes.AssertEqualsToWithXmlSerializer(internalSnapshot);
+        }
+
+        public override void SetUp()
+        {
+            base.SetUp();
+            snapshotConverter = new SnapshotConverter(serializer);
+        }
+
+        private SnapshotConverter snapshotConverter;
+
+        private class InternalSnapshotV1
+        {
+            public int Version { get; set; }
+            public MetaProvider.MetaProviderSnapshot MetaProviderSnapshot { get; set; }
+            public ProcessedTasksCounter.CounterSnapshot CounterSnapshot { get; set; }
+        }
+    }
+}
