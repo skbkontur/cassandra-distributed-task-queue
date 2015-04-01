ebdf18408b87f7bb2de3b0a8dfa8d478aee419f0
tihonove <tihonove@skbkontur.ru>
2015-04-01 12:44:55 +0500

    EDI: Удалил DistributedProfiler с заменой на stats-timer

--- Profiling/GraphiteRemoteTaskQueueProfilerSettings.cs
+++ Profiling/GraphiteRemoteTaskQueueProfilerSettings.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-namespace SKBKontur.Catalogue.RemoteTaskQueue.Profiling
+﻿namespace SKBKontur.Catalogue.RemoteTaskQueue.Profiling
 {
     public class GraphiteRemoteTaskQueueProfilerSettings : IGraphiteRemoteTaskQueueProfilerSettings
     {
-        public TimeSpan AggregationPeriod { get { return TimeSpan.FromMinutes(1); } }
-        public string KeyNamePrefix { get { return "EDI.services.RemoteTaskQueue"; } }
+        public string KeyNamePrefix { get { return "EDI.services.RemoteTaskQueueTasks"; } }
     }
 }
