4cfa805d5b92c8b095017ae029fe247225c08a46
andrew <Andrew.Kostousov@gmail.com>
2015-05-14 16:54:51 +0500

    cleanup dead code

--- TaskMonitoringViewer/Controllers/ProcessingTaskCountModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SKBKontur.Catalogue.RemoteTaskQueue.TaskMonitoringViewer.Controllers
-{
-    public class ProcessingTaskCountModel
-    {
-        public int Count { get; set; }
-        public long UpdateTimeJsTicks { get; set; }
-        public long StartTimeJsTicks { get; set; }
-    }
-}
