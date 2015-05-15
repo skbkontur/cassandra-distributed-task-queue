b9cc7f3dae93ac5baaa250c640b282aeb37f71c1
andrew <Andrew.Kostousov@gmail.com>
2015-05-15 19:32:50 +0500

    extract TasksBaseControllerImpl

--- ElasticMonitoring.MvcControllers/Controllers/TasksBaseController.cs
+++ ElasticMonitoring.MvcControllers/Controllers/TasksBaseController.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -10,15 +7,10 @@ using System.Web.Routing;
 using Humanizer;
 
 using RemoteQueue.Cassandra.Entities;
-using RemoteQueue.Handling;
+using RemoteQueue.UserClasses;
 
 using SKBKontur.Catalogue.Core.Web.Controllers;
-using SKBKontur.Catalogue.Objects;
-using SKBKontur.Catalogue.Objects.ValueExtracting;
-using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding;
-using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.TaskDetailsTreeView;
 using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Models;
-using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.TaskIndexedStorage.Client;
 
 using ControllerBase = SKBKontur.Catalogue.Core.Web.Controllers.ControllerBase;
 
@@ -26,14 +18,12 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
 {
     public abstract class TasksBaseController : ControllerBase
     {
-        protected TasksBaseController(
-            TasksBaseControllerParameters parameters)
-            : base(parameters.BaseParameters)
+        protected TasksBaseController(ControllerBaseParameters baseParameters, TasksControllerImpl controllerImpl, TaskDataRegistryBase taskDataRegistryBase)
+            : base(baseParameters)
         {
-            taskSearchClient = parameters.TaskSearchClient;
-            taskDataNames = parameters.TaskDataRegistryBase.GetAllTaskDataInfos().OrderBy(x => x.Value).Select(x => x.Value).Distinct().ToArray();
+            this.controllerImpl = controllerImpl;
+            taskDataNames = taskDataRegistryBase.GetAllTaskDataInfos().OrderBy(x => x.Value).Select(x => x.Value).Distinct().ToArray();
             taskStates = Enum.GetValues(typeof(TaskState)).Cast<TaskState>().Select(x => new KeyValuePair<string, string>(x.ToString(), x.Humanize())).ToArray();
-            remoteTaskQueue = parameters.RemoteTaskQueue;
         }
 
         [AcceptVerbs(HttpVerbs.Get)]
@@ -41,8 +31,8 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
         {
             CheckReadAccess();
 
-            var rangeStart = ParseDateTime(start);
-            var rangeEnd = ParseDateTime(end);
+            var rangeStart = controllerImpl.ParseDateTime(start);
+            var rangeEnd = controllerImpl.ParseDateTime(end);
             var taskSearchConditions = new TaskSearchConditionsModel
                 {
                     SearchString = q,
@@ -60,7 +50,7 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
             if(string.IsNullOrWhiteSpace(q))
                 return RedirectToAction("Run", new RouteValueDictionary(new {q = "*", start, end}).AppendArray(name, "name").AppendArray(state, "state"));
 
-            var taskSearchResultsModel = BuildResultsBySearchConditions(taskSearchConditions);
+            var taskSearchResultsModel = controllerImpl.BuildResultsBySearchConditions(taskSearchConditions, CurrentUserHasAccessToWriteAction(), CurrentUserHasAccessToTaskData());
             return View("SearchResultsPage", new TasksResultModel
                 {
                     SearchConditions = taskSearchConditions,
@@ -68,22 +58,11 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
                 });
         }
 
-        public static byte[] Compress(byte[] raw)
-        {
-            using(var memory = new MemoryStream())
-            {
-                using(var gzip = new GZipStream(memory, CompressionMode.Compress, true))
-                    gzip.Write(raw, 0, raw.Length);
-
-                return memory.ToArray();
-            }
-        }
-
         [AcceptVerbs(HttpVerbs.Post)]
         public ViewResult Scroll(string iteratorContext)
         {
             CheckReadAccess();
-            var taskSearchResultsModel = BuildResultsByIteratorContext(iteratorContext);
+            var taskSearchResultsModel = controllerImpl.BuildResultsByIteratorContext(iteratorContext, CurrentUserHasAccessToWriteAction(), CurrentUserHasAccessToTaskData());
             return View("Scroll", taskSearchResultsModel);
         }
 
@@ -91,49 +70,24 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
         public ViewResult Details(string id)
         {
             CheckReadAccess();
-            var taskData = remoteTaskQueue.GetTaskInfo(id);
-            return View("TaskDetails2", new TaskDetailsModel
-                {
-                    AllowControlTaskExecution = CurrentUserHasAccessToWriteAction(),
-                    TaskName = taskData.Context.Name,
-                    TaskId = taskData.Context.Id,
-                    State = taskData.Context.State,
-                    EnqueueTime = new DateTime(taskData.Context.Ticks, DateTimeKind.Utc),
-                    StartExecutedTime = TickToDateTime(taskData.Context.StartExecutingTicks),
-                    FinishExecutedTime = TickToDateTime(taskData.Context.FinishExecutingTicks),
-                    MinimalStartTime = TickToDateTime(taskData.Context.MinimalStartTicks),
-                    ParentTaskId = taskData.Context.ParentTaskId,
-                    ChildTaskIds = remoteTaskQueue.GetChildrenTaskIds(taskData.Context.Id),
-                    ExceptionInfo = taskData.With(x => x.ExceptionInfo).Return(x => x.ExceptionMessageInfo, ""),
-                    AttemptCount = taskData.Context.Attempts,
-                    DetailsTree = BuildDetailsTree(taskData, id)
-                });
+            var detailsModel = controllerImpl.Details(id, Url, CurrentUserHasAccessToWriteAction(), CurrentUserHasAccessToTaskData());
+            return View("TaskDetails2", detailsModel);
         }
 
         [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult GetBytes(string id, string path)
         {
             CheckReadAccess();
-            var taskData = remoteTaskQueue.GetTaskInfo(id).TaskData;
-            var value = ObjectValueExtractor.Extract(taskData.GetType(), taskData, path);
-            if(value.GetType() != typeof(byte[]))
-                throw new Exception(string.Format("Type of property by path '{0}' has type '{1}' instead of '{2}'", path, value.GetType(), typeof(byte[])));
-            var fileDownloadName = string.Format("{0}_{1}_{2}.data", DateTime.UtcNow.ToString("yyyy.MM.dd hh:mm:ss"), path, id);
-            return File((byte[])value, "application/octet-stream", fileDownloadName);
-        }
-
-        private static DateTime? TickToDateTime(long? startExecutingTicks)
-        {
-            if(!startExecutingTicks.HasValue)
-                return null;
-            return new DateTime(startExecutingTicks.Value, DateTimeKind.Utc);
+            string fileDownloadName;
+            var fileContents = controllerImpl.GetBytes(id, path, out fileDownloadName);
+            return File(fileContents, "application/octet-stream", fileDownloadName);
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
         public ActionResult Cancel(string id)
         {
             CheckWriteAccess();
-            remoteTaskQueue.CancelTask(id);
+            controllerImpl.Cancel(id);
             return Json(new {Success = true});
         }
 
@@ -141,128 +95,29 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
         public ActionResult Rerun(string id)
         {
             CheckWriteAccess();
-            remoteTaskQueue.RerunTask(id, TimeSpan.FromTicks(0));
+            controllerImpl.Rerun(id);
             return Json(new {Success = true});
         }
 
         private void CheckWriteAccess()
         {
-            if (!CurrentUserHasAccessToWriteAction())
+            if(!CurrentUserHasAccessToWriteAction())
                 throw new ForbiddenException(Request.RawUrl, null);
         }
 
         private void CheckReadAccess()
         {
-            if (!CurrentUserHasAccessToReadAction())
+            if(!CurrentUserHasAccessToReadAction())
                 throw new ForbiddenException(Request.RawUrl, null);
         }
 
         protected abstract string GetAdminToolsActions();
-
         protected abstract bool CurrentUserHasAccessToReadAction();
-
         protected abstract bool CurrentUserHasAccessToTaskData();
-
         protected abstract bool CurrentUserHasAccessToWriteAction();
 
-        private ObjectTreeModel BuildDetailsTree(RemoteTaskInfo taskData, string taskId)
-        {
-            if(!CurrentUserHasAccessToTaskData())
-                return null;
-            var builder = new ObjectTreeViewBuilder(new TaskDataBuildersProvider());
-            var result = builder.Build(taskData.TaskData, new TaskDataBuildingContext {TaskId = taskId, UrlHelper = Url});
-            if(result != null)
-                result.Name = "TaskData";
-            return result;
-        }
-
-        private TaskSearchResultsModel BuildResultsByIteratorContext(string iteratorContext)
-        {
-            var taskSearchResponse = taskSearchClient.SearchNext(iteratorContext);
-
-            var tasksIds = taskSearchResponse.Ids;
-            var nextScrollId = taskSearchResponse.NextScrollId;
-            return new TaskSearchResultsModel
-                {
-                    AllowControlTaskExecution = CurrentUserHasAccessToWriteAction(),
-                    AllowViewTaskData = CurrentUserHasAccessToTaskData(),
-                    Tasks = remoteTaskQueue.GetTaskInfos(tasksIds).Select(x => new TaskModel
-                        {
-                            Id = x.Context.Id,
-                            Name = x.Context.Name,
-                            State = x.Context.State,
-                            EnqueueTime = FromTicks(x.Context.Ticks),
-                            StartExecutionTime = FromTicks(x.Context.StartExecutingTicks),
-                            FinishExecutionTime = FromTicks(x.Context.FinishExecutingTicks),
-                            MinimalStartTime = FromTicks(x.Context.MinimalStartTicks),
-                            AttemptCount = x.Context.Attempts,
-                            ParentTaskId = x.Context.ParentTaskId,
-                        }).ToArray(),
-                    IteratorContext = nextScrollId,
-                };
-        }
-
-        private TaskSearchResultsModel BuildResultsBySearchConditions(TaskSearchConditionsModel taskSearchConditions)
-        {
-            if(taskSearchConditions.RangeStart == null)
-                throw new Exception("Range start should be specified");
-            var start = taskSearchConditions.RangeStart.Value.Ticks;
-            var end = (taskSearchConditions.RangeEnd ?? DateTime.UtcNow).Ticks;
-            var taskSearchResponse = taskSearchClient.SearchFirst(new TaskSearchRequest
-                {
-                    FromTicksUtc = start,
-                    ToTicksUtc = end,
-                    QueryString = taskSearchConditions.SearchString,
-                    TaskNames = taskSearchConditions.TaskNames,
-                    TaskStates = taskSearchConditions.TaskStates
-                });
-
-            var tasksIds = taskSearchResponse.Ids;
-            var total = taskSearchResponse.TotalCount;
-            var nextScrollId = taskSearchResponse.NextScrollId;
-
-            var taskSearchResultsModel = new TaskSearchResultsModel
-                {
-                    AllowControlTaskExecution = CurrentUserHasAccessToWriteAction(),
-                    AllowViewTaskData = CurrentUserHasAccessToTaskData(),
-                    Tasks = remoteTaskQueue.GetTaskInfos(tasksIds).Select(x => new TaskModel
-                        {
-                            Id = x.Context.Id,
-                            Name = x.Context.Name,
-                            State = x.Context.State,
-                            EnqueueTime = FromTicks(x.Context.Ticks),
-                            StartExecutionTime = FromTicks(x.Context.StartExecutingTicks),
-                            FinishExecutionTime = FromTicks(x.Context.FinishExecutingTicks),
-                            MinimalStartTime = FromTicks(x.Context.MinimalStartTicks),
-                            AttemptCount = x.Context.Attempts,
-                            ParentTaskId = x.Context.ParentTaskId,
-                        }).ToArray(),
-                    IteratorContext = nextScrollId,
-                    TotalResultCount = total
-                };
-            return taskSearchResultsModel;
-        }
-
-        private static DateTime? ParseDateTime(string start)
-        {
-            if(string.IsNullOrWhiteSpace(start))
-                return null;
-            DateTime result;
-            if(!DateTime.TryParse(start, CultureInfo.GetCultureInfo("ru-RU"), DateTimeStyles.AssumeUniversal, out result))
-                return null;
-            return result;
-        }
-
-        private static DateTime? FromTicks(long? ticks)
-        {
-            if(ticks.HasValue)
-                return new DateTime(ticks.Value, DateTimeKind.Utc);
-            return null;
-        }
-
-        private readonly ITaskSearchClient taskSearchClient;
+        private readonly TasksControllerImpl controllerImpl;
         private readonly string[] taskDataNames;
-        private readonly IRemoteTaskQueue remoteTaskQueue;
         private readonly KeyValuePair<string, string>[] taskStates;
     }
 }
