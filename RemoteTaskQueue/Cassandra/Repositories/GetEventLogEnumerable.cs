c0dc18f0ec2777a21fa01f51cdabaac15bb00017
andrew <Andrew.Kostousov@gmail.com>
2015-08-15 18:44:12 +0500

    move types into matching files

--- RemoteTaskQueue/Cassandra/Repositories/GetEventLogEnumerable.cs
+++ RemoteTaskQueue/Cassandra/Repositories/GetEventLogEnumerable.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 
 using GroBuf;
 
 using RemoteQueue.Cassandra.Entities;
 
-using SKBKontur.Cassandra.CassandraClient.Abstractions;
 using SKBKontur.Cassandra.CassandraClient.Connections;
 
 namespace RemoteQueue.Cassandra.Repositories
@@ -38,76 +35,5 @@ namespace RemoteQueue.Cassandra.Repositories
         private readonly long fromTicks;
         private readonly long toTicks;
         private readonly int batchSize;
-    }
-
-
-
-
-    public class GetEventLogEnumerator : IEnumerator<TaskMetaUpdatedEvent>
-    {
-        public GetEventLogEnumerator(ISerializer serializer, IColumnFamilyConnection connection, long fromTicks, long toTicks, int batchSize)
-        {
-            this.serializer = serializer;
-            this.connection = connection;
-            this.fromTicks = fromTicks;
-            this.batchSize = batchSize;
-            iFrom = fromTicks / tickPartition;
-            iTo = (toTicks + tickPartition / 3) / tickPartition;
-            Reset();
-        }
-
-        public void Dispose()
-        {
-            eventEnumerator.Dispose();
-        }
-
-        public bool MoveNext()
-        {
-            while (true)
-            {
-                if (eventEnumerator.MoveNext()) return true;
-                if (iCur >= iTo) return false;
-                iCur++;
-                string startColumnName = null;
-                var columnInfo = GetColumnInfo(iCur * tickPartition);
-                if (iCur == iFrom) startColumnName = GetColumnInfo(fromTicks).Item2;
-                eventEnumerator = connection.GetRow(columnInfo.Item1, startColumnName, batchSize).GetEnumerator();
-            }
-        }
-
-
-        private static Tuple<string, string> GetColumnInfo(long ticks)
-        {
-            var rowKey = (ticks / tickPartition).ToString();
-            var columnName = ticks.ToString("D20", CultureInfo.InvariantCulture);
-            return new Tuple<string, string>(rowKey, columnName);
-        }
-
-        public void Reset()
-        {
-            iCur = iFrom - 1;
-            eventEnumerator = (new List<Column>()).GetEnumerator();
-        }
-
-        public TaskMetaUpdatedEvent Current
-        {
-            get
-            {
-                var xmlContent = eventEnumerator.Current.Value;
-                return serializer.Deserialize<TaskMetaUpdatedEvent>(xmlContent);
-            }
-        }
-
-        object IEnumerator.Current { get { return Current; } }
-
-        private readonly ISerializer serializer;
-        private readonly IColumnFamilyConnection connection;
-        private readonly long fromTicks;
-        private readonly int batchSize;
-        private readonly long iFrom;
-        private readonly long iTo;
-        private long iCur;
-        private IEnumerator<Column> eventEnumerator;
-        private static readonly long tickPartition = TimeSpan.FromMinutes(6).Ticks;
     }
 }
