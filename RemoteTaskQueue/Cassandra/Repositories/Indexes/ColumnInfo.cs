04dc544ad68d7347ea27a14e38166b89c04a64a4
andrew <Andrew.Kostousov@gmail.com>
2015-10-07 21:30:33 +0500

    no need in custom equality rules for ColumnInfo

--- RemoteTaskQueue/Cassandra/Repositories/Indexes/ColumnInfo.cs
+++ RemoteTaskQueue/Cassandra/Repositories/Indexes/ColumnInfo.cs
@@ -15,28 +15,5 @@ namespace RemoteQueue.Cassandra.Repositories.Indexes
 
         [NotNull]
         public string ColumnName { get; private set; }
-
-        public bool Equals(ColumnInfo other)
-        {
-            if(ReferenceEquals(null, other)) return false;
-            if(ReferenceEquals(this, other)) return true;
-            return Equals(other.RowKey, RowKey) && Equals(other.ColumnName, ColumnName);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if(ReferenceEquals(null, obj)) return false;
-            if(ReferenceEquals(this, obj)) return true;
-            if(obj.GetType() != typeof(ColumnInfo)) return false;
-            return Equals((ColumnInfo)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (RowKey.GetHashCode() * 397) ^ ColumnName.GetHashCode();
-            }
-        }
     }
 }
