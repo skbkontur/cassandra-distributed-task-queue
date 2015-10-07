422502181239baa00019fcf85d16d582ff6065b2
andrew <Andrew.Kostousov@gmail.com>
2015-10-07 19:58:32 +0500

    refactor TicksHolder to expose atomic read / write ops

--- FunctionalTests/RepositoriesTests/TicksHolderTest.cs
+++ FunctionalTests/RepositoriesTests/TicksHolderTest.cs
@@ -18,11 +18,11 @@ namespace FunctionalTests.RepositoriesTests
         public void UpdateMaxTicks()
         {
             var ticks = DateTime.UtcNow.Ticks;
-            Assert.AreEqual(ticks, ticksHolder.UpdateMaxTicks("r", ticks));
-            Assert.AreEqual(ticks + 2, ticksHolder.UpdateMaxTicks("r1", ticks + 2));
-            Assert.AreEqual(ticks + 2, ticksHolder.UpdateMaxTicks("r1", ticks + 1));
+            Assert.AreEqual(ticks, UpdateMaxTicks("r", ticks));
+            Assert.AreEqual(ticks + 2, UpdateMaxTicks("r1", ticks + 2));
+            Assert.AreEqual(ticks + 2, UpdateMaxTicks("r1", ticks + 1));
             Assert.AreEqual(ticks, ticksHolder.GetMaxTicks("r"));
-            Assert.AreEqual(ticks + 2, ticksHolder.UpdateMaxTicks("r", ticks + 2));
+            Assert.AreEqual(ticks + 2, UpdateMaxTicks("r", ticks + 2));
             Assert.AreEqual(ticks + 2, ticksHolder.GetMaxTicks("r"));
         }
 
@@ -36,6 +36,12 @@ namespace FunctionalTests.RepositoriesTests
             Assert.AreEqual(ticks, ticksHolder.GetMinTicks("r"));
             Assert.AreEqual(ticks - 2, UpdateMinTicks("r", ticks - 2));
             Assert.AreEqual(ticks - 2, ticksHolder.GetMinTicks("r"));
+        }
+
+        private long UpdateMaxTicks(string name, long ticks)
+        {
+            ticksHolder.UpdateMaxTicks(name, ticks);
+            return ticksHolder.GetMaxTicks(name);
         }
 
         private long UpdateMinTicks(string name, long ticks)
