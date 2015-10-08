d39daa548f734ee40686e664138512cbce1f3705
andrew <Andrew.Kostousov@gmail.com>
2015-10-08 12:46:24 +0500

    TaskColumnInfo should be equatable (see HandleTasksMetaStorage.AddMeta())

--- RemoteTaskQueue/Cassandra/Repositories/Indexes/TaskColumnInfo.cs
+++ RemoteTaskQueue/Cassandra/Repositories/Indexes/TaskColumnInfo.cs
@@ -1,8 +1,10 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+
+using JetBrains.Annotations;
 
 namespace RemoteQueue.Cassandra.Repositories.Indexes
 {
-    public class TaskColumnInfo
+    public class TaskColumnInfo : IEquatable<TaskColumnInfo>
     {
         public TaskColumnInfo([NotNull] string rowKey, [NotNull] string columnName)
         {
@@ -15,5 +17,33 @@ namespace RemoteQueue.Cassandra.Repositories.Indexes
 
         [NotNull]
         public string ColumnName { get; private set; }
+
+        public bool Equals(TaskColumnInfo other)
+        {
+            if(ReferenceEquals(null, other)) return false;
+            if(ReferenceEquals(this, other)) return true;
+            return string.Equals(RowKey, other.RowKey) && string.Equals(ColumnName, other.ColumnName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(ReferenceEquals(null, obj)) return false;
+            if(ReferenceEquals(this, obj)) return true;
+            if(obj.GetType() != this.GetType()) return false;
+            return Equals((TaskColumnInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (RowKey.GetHashCode() * 397) ^ ColumnName.GetHashCode();
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("RowKey: {0}, ColumnName: {1}", RowKey, ColumnName);
+        }
     }
 }
