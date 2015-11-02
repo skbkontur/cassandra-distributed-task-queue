93a969254aa8a95019f9e422376573a00cbe751e
ikulaga <ikulaga@skbkontur.ru>
2015-11-02 14:39:59 +0500

    Реализовал OrderedBlobStorage + тесты

---
+++ FunctionalTests/RepositoriesTests/OrderedBlobStorageTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GroBuf;
+
+using NUnit.Framework;
+
+using RemoteQueue.Cassandra.Primitives;
+using RemoteQueue.Cassandra.Repositories.BlobStorages;
+using RemoteQueue.Cassandra.Repositories.GlobalTicksHolder;
+using RemoteQueue.Settings;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Clusters;
+using SKBKontur.Catalogue.Objects;
+
+namespace FunctionalTests.RepositoriesTests
+{
+    public class OrderedBlobStorageTest : FunctionalTestBaseWithoutServices
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+            var repositoryParameters = Container.Get<IColumnFamilyRepositoryParameters>();
+            var serializer = Container.Get<ISerializer>();
+            var globalTime = Container.Get<IGlobalTime>();
+            var cassandraCluster = Container.Get<ICassandraCluster>();
+            var settings = Container.Get<ICassandraSettings>();
+            const string columnFamilyName = "OrderedBlobStorageTest";
+            var connection = cassandraCluster.RetrieveKeyspaceConnection(settings.QueueKeyspace);
+            var keyspace = connection.DescribeKeyspace();
+            if(!keyspace.ColumnFamilies.Any(x => x.Key == columnFamilyName))
+            {
+                connection.AddColumnFamily(new ColumnFamily
+                    {
+                        Name = columnFamilyName,
+                        GCGraceSeconds = 10,
+                        Caching = ColumnFamilyCaching.All
+                    });
+            }
+            orderedBlobStorage = new OrderedBlobStorage<int?>(repositoryParameters, serializer, globalTime, columnFamilyName);
+        }
+
+        [Test]
+        public void TestInvalidMethodCallsWithoutTimeGuid()
+        {
+            const string id = "some-string";
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.Write(id, 1));
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.Read(id));
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.Write(new[] { new KeyValuePair<string, int?>(id, 1)}));
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.ReadQuiet(new[] { id }));
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.Delete(id, 100));
+            Assert.Throws<ArgumentException>(() => orderedBlobStorage.Delete(new []{id}, 100));
+        }
+
+        [Test]
+        public void TestReadWrite()
+        {
+            var id = TimeGuid.NowGuid().ToGuid().ToString();
+            Assert.IsNull(orderedBlobStorage.Read(id));
+
+            orderedBlobStorage.Write(id, 10);
+            Assert.That(orderedBlobStorage.Read(id), Is.EqualTo(10));
+
+            orderedBlobStorage.Write(id, 11);
+            Assert.That(orderedBlobStorage.Read(id), Is.EqualTo(11));
+        }
+
+        [Test]
+        public void TestMultiWrite()
+        {
+            var id1 = TimeGuid.NowGuid().ToGuid().ToString();
+            var id2 = TimeGuid.NowGuid().ToGuid().ToString();
+
+            orderedBlobStorage.Write(new []{new KeyValuePair<string, int?>(id1, 1), new KeyValuePair<string, int?>(id2, 2)});
+            Assert.That(orderedBlobStorage.Read(id1), Is.EqualTo(1));
+            Assert.That(orderedBlobStorage.Read(id2), Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void TestReadQuiet()
+        {
+            var id1 = TimeGuid.NowGuid().ToGuid().ToString();
+            var id2 = TimeGuid.NowGuid().ToGuid().ToString();
+
+            Assert.That(orderedBlobStorage.ReadQuiet(new []{id1, id2}), Is.EquivalentTo(new int?[]{null, null}));
+
+            orderedBlobStorage.Write(id1, 1);
+            Assert.That(orderedBlobStorage.ReadQuiet(new[] { id1, id2 }), Is.EqualTo(new int?[] { 1, null }));
+            Assert.That(orderedBlobStorage.ReadQuiet(new[] { id2, id1 }), Is.EqualTo(new int?[] { null, 1 }));
+
+            orderedBlobStorage.Write(id2, 2);
+            Assert.That(orderedBlobStorage.ReadQuiet(new[] { id1, id2 }), Is.EqualTo(new int?[] { 1, 2 }));
+            Assert.That(orderedBlobStorage.ReadQuiet(new[] { id2, id1 }), Is.EqualTo(new int?[] { 2, 1 }));
+        }
+
+        [Test]
+        public void TestMultiRead()
+        {
+            var id1 = TimeGuid.NowGuid().ToGuid().ToString();
+            var id2 = TimeGuid.NowGuid().ToGuid().ToString();
+
+            Assert.That(orderedBlobStorage.Read(new []{id1, id2}).Length, Is.EqualTo(0));
+
+            orderedBlobStorage.Write(id1, 1);
+            Assert.That(orderedBlobStorage.Read(new[] { id1, id2 }), Is.EqualTo(new int?[]{1}));
+
+            orderedBlobStorage.Write(id2, 2);
+            Assert.That(orderedBlobStorage.Read(new[] {id1, id2}), Is.EqualTo(new int?[] { 1, 2 }));
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            var id = TimeGuid.NowGuid().ToGuid().ToString();
+
+            orderedBlobStorage.Write(id, 1);
+            orderedBlobStorage.Delete(id, DateTime.UtcNow.Ticks);
+
+            Assert.IsNull(orderedBlobStorage.Read(id));
+        }
+
+        [Test]
+        public void TestMultiDelete()
+        {
+            var id1 = TimeGuid.NowGuid().ToGuid().ToString();
+            var id2 = TimeGuid.NowGuid().ToGuid().ToString();
+
+            orderedBlobStorage.Write(id1, 1);
+            orderedBlobStorage.Write(id2, 2);
+
+            orderedBlobStorage.Delete(new []{id1, id2}, DateTime.UtcNow.Ticks);
+            Assert.That(orderedBlobStorage.Read(new []{id1, id2}).Length, Is.EqualTo(0));
+        }
+
+        private OrderedBlobStorage<int?> orderedBlobStorage;
+    }
+}
---
+++ RemoteTaskQueue/Cassandra/Repositories/BlobStorages/OrderedBlobStorage.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GroBuf;
+
+using JetBrains.Annotations;
+
+using MoreLinq;
+
+using RemoteQueue.Cassandra.Primitives;
+using RemoteQueue.Cassandra.Repositories.GlobalTicksHolder;
+using RemoteQueue.Cassandra.Repositories.Indexes;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Connections;
+using SKBKontur.Catalogue.Objects;
+using SKBKontur.Catalogue.Objects.TimeBasedUuid;
+
+namespace RemoteQueue.Cassandra.Repositories.BlobStorages
+{
+    public class OrderedBlobStorage<T> : ColumnFamilyRepositoryBase, IBlobStorage<T>
+    {
+        public OrderedBlobStorage(IColumnFamilyRepositoryParameters parameters, ISerializer serializer, IGlobalTime globalTime, string columnFamilyName)
+            : base(parameters, columnFamilyName)
+        {
+            this.serializer = serializer;
+            this.globalTime = globalTime;
+        }
+
+        public void Write([NotNull] string id, T element)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            var nowTicks = globalTime.UpdateNowTicks();
+            var columnInfo = GetColumnInfo(id);
+
+            connection.AddColumn(columnInfo.RowKey, new Column
+                {
+                    Name = columnInfo.ColumnName,
+                    Timestamp = nowTicks,
+                    Value = serializer.Serialize(element)
+                });
+        }
+
+        public void Write(KeyValuePair<string, T>[] elements)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            var updateNowTicks = globalTime.UpdateNowTicks();
+
+            connection.BatchInsert(elements.Select(x =>
+                {
+                    var columnInfo = GetColumnInfo(x.Key);
+                    return new KeyValuePair<string, IEnumerable<Column>>(
+                        columnInfo.RowKey,
+                        new[]
+                            {
+                                new Column
+                                    {
+                                        Name = columnInfo.ColumnName,
+                                        Timestamp = updateNowTicks,
+                                        Value = serializer.Serialize(x.Value)
+                                    }
+                            });
+                }));
+        }
+
+        public T Read([NotNull] string id)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            var columnInfo = GetColumnInfo(id);
+
+            Column column;
+            if(connection.TryGetColumn(columnInfo.RowKey, columnInfo.ColumnName, out column))
+                return serializer.Deserialize<T>(column.Value);
+            return default(T);
+        }
+
+        //may be rename to Select?
+        public T[] Read([NotNull]string[] ids)
+        {
+            return SelectColumns(ids).Select(column => serializer.Deserialize<T>(column.Value)).ToArray();
+        }
+
+        public T[] ReadQuiet([NotNull]string[] ids)
+        {
+            var result = new T[ids.Length];
+            var objectsMap  = SelectColumns(ids).ToDictionary(x => x.Name);
+            for(var i = 0; i < ids.Length; i++)
+            {
+                if (objectsMap.ContainsKey(ids[i]))
+                    result[i] = serializer.Deserialize<T>(objectsMap[ids[i]].Value);
+            }
+            return result;
+        }
+
+        public IEnumerable<T> ReadAll(int batchSize = 1000)
+        {
+            throw new NotSupportedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, T>> ReadAllWithIds(int batchSize = 1000)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Delete([NotNull] string id, long timestamp)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            var columnInfo = GetColumnInfo(id);
+
+            connection.DeleteColumn(columnInfo.RowKey, columnInfo.ColumnName, timestamp);
+        }
+
+        public void Delete([NotNull] string[] ids, long? timestamp)
+        {
+            ids
+                .Select(GetColumnInfo)
+                .GroupBy(x => x.RowKey)
+                .ForEach(group =>
+                    {
+                        group
+                            .Batch(1000, Enumerable.ToArray)
+                            .ForEach(columnInfo => MakeInConnection(connection => connection.DeleteBatch(group.Key, columnInfo.Select(x => x.ColumnName).Distinct().ToArray(), timestamp)));
+                    });
+        }
+
+        private IEnumerable<Column> SelectColumns([NotNull] string[] ids)
+        {
+            if(!ids.Any())
+                return new Column[0];
+
+            return ids
+                .Select(GetColumnInfo)
+                .GroupBy(x => x.RowKey)
+                .SelectMany(group =>
+                    {
+                        return group
+                            .Batch(1000, Enumerable.ToArray)
+                            .SelectMany(columnInfo => MakeInConnection(connection => connection.GetColumns(group.Key, columnInfo.Select(x => x.ColumnName).Distinct().ToArray())));
+                    })
+                .Where(x => x.Value != null);
+        }
+
+        private void MakeInConnection(Action<IColumnFamilyConnection> action)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            action(connection);
+        }
+
+        private TResult MakeInConnection<TResult>(Func<IColumnFamilyConnection, TResult> action)
+        {
+            var connection = RetrieveColumnFamilyConnection();
+            return action(connection);
+        }
+
+        private static ColumnInfo GetColumnInfo(string id)
+        {
+            TimeGuid timeGuid;
+            if (!TryGetTimeGuid(id, out timeGuid))
+                throw new ArgumentException("Parameter should be TimeGuid.");
+
+            var ticks = timeGuid.GetTimestamp().Ticks;
+            var rowKey = (ticks / tickPartition).ToString();
+
+            return new ColumnInfo{RowKey = rowKey, ColumnName = id};
+        }
+
+        private static bool TryGetTimeGuid(string input, out TimeGuid timeGuid)
+        {
+            timeGuid = null;
+            Guid guid;
+            if (!Guid.TryParse(input, out guid))
+                return false;
+            if (TimeGuidFormatter.GetVersion(guid) != GuidVersion.TimeBased)
+                return false;
+
+            timeGuid = new TimeGuid(guid);
+            return true;
+        }
+
+        private static readonly long tickPartition = TimeSpan.FromMinutes(6).Ticks;
+        private readonly ISerializer serializer;
+        private readonly IGlobalTime globalTime;
+    }
+}
