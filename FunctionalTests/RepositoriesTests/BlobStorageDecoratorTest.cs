c072c8d676fd28d9f17a2e8f5737d8f68f9f6dfd
ikulaga <ikulaga@skbkontur.ru>
2015-11-02 15:27:23 +0500

    Реализовал BlobStorageDecorator + тесты на него

---
+++ FunctionalTests/RepositoriesTests/BlobStorageDecoratorTest.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Linq;
+
+using GroBuf;
+
+using NUnit.Framework;
+
+using RemoteQueue.Cassandra.Primitives;
+using RemoteQueue.Cassandra.Repositories.BlobStorages;
+using RemoteQueue.Cassandra.Repositories.GlobalTicksHolder;
+using RemoteQueue.Settings;
+
+using SKBKontur.Cassandra.CassandraClient.Abstractions;
+using SKBKontur.Cassandra.CassandraClient.Clusters;
+using SKBKontur.Cassandra.CassandraClient.Connections;
+using SKBKontur.Catalogue.Objects;
+
+namespace FunctionalTests.RepositoriesTests
+{
+    public class BlobStorageDecoratorTest : FunctionalTestBaseWithoutServices
+    {
+        private const string blobStorageColumnFamilyName = "blobStorageTest";
+        private const string orderedBlobStorageColumnFamilyName = "orderedBlobStorageTest";
+
+        public override void SetUp()
+        {
+            base.SetUp();
+            var repositoryParameters = Container.Get<IColumnFamilyRepositoryParameters>();
+            var serializer = Container.Get<ISerializer>();
+            var globalTime = Container.Get<IGlobalTime>();
+            var cassandraCluster = Container.Get<ICassandraCluster>();
+            var settings = Container.Get<ICassandraSettings>();
+
+            var connection = cassandraCluster.RetrieveKeyspaceConnection(settings.QueueKeyspace);
+            AddColumnFamily(connection, blobStorageColumnFamilyName);
+            AddColumnFamily(connection, orderedBlobStorageColumnFamilyName);
+
+            blobStorageDecorator = new BlobStorageDecorator<int?>(repositoryParameters, serializer, globalTime, blobStorageColumnFamilyName, orderedBlobStorageColumnFamilyName);
+            blobStorage = new BlobStorage<int?>(repositoryParameters, serializer, globalTime, blobStorageColumnFamilyName);
+            orderedBlobStorage = new OrderedBlobStorage<int?>(repositoryParameters, serializer, globalTime, orderedBlobStorageColumnFamilyName);
+        }
+
+        private void AddColumnFamily(IKeyspaceConnection connection, string columnFamilyName)
+        {
+            var keyspace = connection.DescribeKeyspace();
+            if(!keyspace.ColumnFamilies.Any(x => x.Key == columnFamilyName))
+            {
+                connection.AddColumnFamily(new ColumnFamily
+                    {
+                        Name = columnFamilyName,
+                        GCGraceSeconds = 10,
+                        Caching = ColumnFamilyCaching.All
+                    });
+            }
+        }
+
+        [Test]
+        public void TestRead()
+        {
+            blobStorage.Write(stringId, 1);
+            Assert.That(blobStorageDecorator.Read(stringId), Is.EqualTo(1));
+
+            orderedBlobStorage.Write(timeGuidId, 2);
+            Assert.That(blobStorageDecorator.Read(stringId), Is.EqualTo(1));
+            Assert.That(blobStorageDecorator.Read(timeGuidId), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestWrite()
+        {
+            blobStorageDecorator.Write(stringId, 1);
+            Assert.That(blobStorage.Read(stringId), Is.EqualTo(1));
+
+            blobStorageDecorator.Write(timeGuidId, 2);
+            Assert.That(orderedBlobStorage.Read(timeGuidId), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestMultiRead()
+        {
+            blobStorage.Write(stringId, 1);
+            Assert.That(blobStorageDecorator.Read(new[] { stringId, timeGuidId }), Is.EqualTo(new int?[] { 1 }));
+
+            orderedBlobStorage.Write(timeGuidId, 2);
+            Assert.That(blobStorageDecorator.Read(new[] {stringId, timeGuidId}), Is.EquivalentTo(new int?[] {1, 2}));
+        }
+
+        [Test]
+        public void TestReadQuiet()
+        {
+            blobStorage.Write(stringId, 1);
+            Assert.That(blobStorageDecorator.ReadQuiet(new[] { stringId, timeGuidId }), Is.EqualTo(new int?[] { 1, null }));
+
+            orderedBlobStorage.Write(timeGuidId, 2);
+            Assert.That(blobStorageDecorator.ReadQuiet(new[] { stringId, timeGuidId }), Is.EqualTo(new int?[] { 1, 2 }));
+            Assert.That(blobStorageDecorator.ReadQuiet(new[] { timeGuidId, stringId }), Is.EqualTo(new int?[] { 2, 1 }));
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            blobStorage.Write(stringId, 1);
+            blobStorageDecorator.Delete(stringId, DateTime.UtcNow.Ticks);
+            Assert.IsNull(blobStorage.Read(stringId));
+
+            orderedBlobStorage.Write(timeGuidId, 2);
+            blobStorageDecorator.Delete(timeGuidId, DateTime.UtcNow.Ticks);
+            Assert.IsNull(orderedBlobStorage.Read(timeGuidId));
+        }
+
+        [Test]
+        public void TestMultiDelete()
+        {
+            blobStorage.Write(stringId, 1);
+            orderedBlobStorage.Write(timeGuidId, 2);
+            blobStorageDecorator.Delete(new []{stringId, timeGuidId}, DateTime.UtcNow.Ticks);
+
+            Assert.IsNull(blobStorage.Read(stringId));
+            Assert.IsNull(orderedBlobStorage.Read(timeGuidId));
+        }
+
+        private const string stringId = "stringId";
+        private BlobStorageDecorator<int?> blobStorageDecorator;
+        private BlobStorage<int?> blobStorage;
+        private OrderedBlobStorage<int?> orderedBlobStorage;
+        private readonly string timeGuidId = TimeGuid.NowGuid().ToGuid().ToString();
+    }
+}
---
+++ RemoteTaskQueue/Cassandra/Repositories/BlobStorages/BlobStorageDecorator.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GroBuf;
+
+using RemoteQueue.Cassandra.Primitives;
+using RemoteQueue.Cassandra.Repositories.GlobalTicksHolder;
+
+using SKBKontur.Catalogue.Objects.TimeBasedUuid;
+
+namespace RemoteQueue.Cassandra.Repositories.BlobStorages
+{
+    public class BlobStorageDecorator<T> : IBlobStorage<T>
+    {
+        public BlobStorageDecorator(IColumnFamilyRepositoryParameters parameters, ISerializer serializer, IGlobalTime globalTime, string columnFamilyName)
+            : this(parameters, serializer, globalTime, columnFamilyName, string.Format("ordered{0}", columnFamilyName))
+        {
+        }
+
+        public BlobStorageDecorator(IColumnFamilyRepositoryParameters parameters, ISerializer serializer, IGlobalTime globalTime, string columnFamilyName, string orderedColumnFamilyName)
+        {
+            blobStorage = new BlobStorage<T>(parameters, serializer, globalTime, columnFamilyName);
+            orderedBlobStorage = new OrderedBlobStorage<T>(parameters, serializer, globalTime, orderedColumnFamilyName);
+        }
+
+        public void Write(string id, T element)
+        {
+            if(IsTimeGuid(id))
+                orderedBlobStorage.Write(id, element);
+            else
+                blobStorage.Write(id, element);
+        }
+
+        public void Write(KeyValuePair<string, T>[] elements)
+        {
+            var parts = Split(elements, element => element.Key);
+
+            if(parts.Item1.Any())
+                orderedBlobStorage.Write(parts.Item1);
+
+            if(parts.Item2.Any())
+                blobStorage.Write(parts.Item2);
+        }
+
+        public T Read(string id)
+        {
+            return IsTimeGuid(id) ? orderedBlobStorage.Read(id) : blobStorage.Read(id);
+        }
+
+        public T[] Read(string[] ids)
+        {
+            var parts = Split(ids, id => id);
+            return orderedBlobStorage.Read(parts.Item1)
+                                     .Concat(blobStorage.Read(parts.Item2))
+                                     .ToArray();
+        }
+
+        public T[] ReadQuiet(string[] ids)
+        {
+            var parts = Split(ids, id => id);
+            var orderedEntities = orderedBlobStorage.ReadQuiet(parts.Item1);
+            var orderedEntitiesMap = parts.Item1.Zip(orderedEntities, (id, entity) => new KeyValuePair<string, T>(id, entity)).ToDictionary(x => x.Key, x => x.Value);
+
+            var entities = blobStorage.ReadQuiet(parts.Item2);
+            var entitiesMap = parts.Item2.Zip(entities, (id, entity) => new KeyValuePair<string, T>(id, entity)).ToDictionary(x => x.Key, x => x.Value);
+
+            var result = new T[ids.Length];
+            for(var i = 0; i < ids.Length; i++)
+            {
+                var id = ids[i];
+                if(orderedEntitiesMap.ContainsKey(id))
+                {
+                    result[i] = orderedEntitiesMap[id];
+                    continue;
+                }
+                if(entitiesMap.ContainsKey(id))
+                    result[i] = entitiesMap[id];
+            }
+
+            return result;
+        }
+
+        public IEnumerable<T> ReadAll(int batchSize = 1000)
+        {
+            throw new NotSupportedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, T>> ReadAllWithIds(int batchSize = 1000)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Delete(string id, long timestamp)
+        {
+            if(IsTimeGuid(id))
+                orderedBlobStorage.Delete(id, timestamp);
+            else
+                blobStorage.Delete(id, timestamp);
+        }
+
+        public void Delete(string[] ids, long? timestamp)
+        {
+            var parts = Split(ids, id => id);
+            if(parts.Item1.Any())
+                orderedBlobStorage.Delete(parts.Item1, timestamp);
+
+            if(parts.Item2.Any())
+                blobStorage.Delete(parts.Item2, timestamp);
+        }
+
+        private static bool IsTimeGuid(string input)
+        {
+            Guid guid;
+            return Guid.TryParse(input, out guid) && TimeGuidFormatter.GetVersion(guid) == GuidVersion.TimeBased;
+        }
+
+        private static Tuple<TItem[], TItem[]> Split<TItem>(IEnumerable<TItem> items, Func<TItem, string> getId)
+        {
+            var orderedBlobIds = new List<TItem>();
+            var blobIds = new List<TItem>();
+            foreach(var item in items)
+            {
+                var id = getId(item);
+                if(IsTimeGuid(id))
+                    orderedBlobIds.Add(item);
+                else
+                    blobIds.Add(item);
+            }
+
+            return new Tuple<TItem[], TItem[]>(orderedBlobIds.ToArray(), blobIds.ToArray());
+        }
+
+        private readonly IBlobStorage<T> blobStorage;
+        private readonly IBlobStorage<T> orderedBlobStorage;
+    }
+}
