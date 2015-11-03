6a17aa08e69aa64dfafe3ce5d64baa2e7bf612cc
andrew <Andrew.Kostousov@gmail.com>
2015-11-03 20:41:19 +0500

    cache reflection results

--- RemoteTaskQueue/Cassandra/Entities/CassandraNameAttribute.cs
+++ RemoteTaskQueue/Cassandra/Entities/CassandraNameAttribute.cs
@@ -1,15 +1,18 @@
 using System;
 
+using JetBrains.Annotations;
+
 namespace RemoteQueue.Cassandra.Entities
 {
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
     public sealed class CassandraNameAttribute : Attribute
     {
-        public CassandraNameAttribute(string name)
+        public CassandraNameAttribute([NotNull] string name)
         {
             Name = name;
         }
 
+        [NotNull]
         public string Name { get; private set; }
     }
 }
