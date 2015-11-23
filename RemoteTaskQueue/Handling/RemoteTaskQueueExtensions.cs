8894a78a8a90b36469a3625ec9c6f310d135d576
andrew <Andrew.Kostousov@gmail.com>
2015-11-23 16:49:56 +0500

    + ContinueWith extension for RemoteTaskQueue

---
+++ RemoteTaskQueue/Handling/RemoteTaskQueueExtensions.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace RemoteQueue.Handling
+{
+    public static class RemoteTaskQueueExtensions
+    {
+        [NotNull]
+        public static string ContinueWith<T>([NotNull] this IRemoteTaskQueue remoteTaskQueue, [NotNull] T taskData) where T : ITaskData
+        {
+            return remoteTaskQueue.CreateTask(taskData).Queue();
+        }
+    }
+}
