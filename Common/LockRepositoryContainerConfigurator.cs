9ee5336ef87d7edd85553006860de1531935fd2d
burmistrov <burmistrov@skbkontur.ru>
2016-08-09 11:30:24 +0500

    Fixes in Core tests regarding remote lock migration

--- Common/LockRepositoryContainerConfigurator.cs
+++ Common/LockRepositoryContainerConfigurator.cs
@@ -5,9 +5,11 @@ using GroboContainer.Core;
 
 using RemoteQueue.Cassandra.Primitives;
 
+using SKBKontur.Cassandra.CassandraClient.Clusters;
 using SKBKontur.Catalogue.CassandraPrimitives.RemoteLock;
 using SKBKontur.Catalogue.CassandraPrimitives.RemoteLock.RemoteLocker;
 using SKBKontur.Catalogue.CassandraPrimitives.Storages.Primitives;
+using SKBKontur.Catalogue.RemoteTaskQueue.Common.RemoteTaskQueue;
 
 namespace SKBKontur.Catalogue.RemoteTaskQueue.Common
 {
@@ -16,6 +18,7 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.Common
         public static void ConfigureLockRepository(this IContainer container, string remoteLockColumnFamily = null)
         {
             var serializer = container.Get<ISerializer>();
+            container.Configurator.ForAbstraction<ICassandraClusterSettings>().UseType<RemoteQueueTestsCassandraSettings>();
             var columnFamilyRepositoryParameters = container.Get<IColumnFamilyRepositoryParameters>();
             remoteLockColumnFamily = remoteLockColumnFamily ?? columnFamilyRepositoryParameters.LockColumnFamilyName;
             var cassandraRemoteLockImplementationSettings = CassandraRemoteLockImplementationSettings.Default(new ColumnFamilyFullName(columnFamilyRepositoryParameters.Settings.QueueKeyspaceForLock, remoteLockColumnFamily));
