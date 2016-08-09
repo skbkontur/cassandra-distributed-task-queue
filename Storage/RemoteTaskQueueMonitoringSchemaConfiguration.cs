09a1e45fe9fc396742a4ac9314c50e5a05b24c51
andrew <Andrew.Kostousov@gmail.com>
2016-08-09 17:13:11 +0500

    simplify BOS event log configuration code

--- Storage/RemoteTaskQueueMonitoringSchemaConfiguration.cs
+++ Storage/RemoteTaskQueueMonitoringSchemaConfiguration.cs
@@ -3,7 +3,9 @@
 using RemoteQueue.Cassandra.Primitives;
 
 using SKBKontur.Catalogue.CassandraStorageCore;
+using SKBKontur.Catalogue.CassandraStorageCore.EventLog;
 using SKBKontur.Catalogue.CassandraStorageCore.Storage.BusinessObjects.Schema;
+using SKBKontur.Catalogue.CassandraStorageCore.Storage.Persistent;
 using SKBKontur.Catalogue.CassandraStorageCore.Storage.Persistent.Cassandra.Schema;
 using SKBKontur.Catalogue.RemoteTaskQueue.MonitoringDataTypes.MonitoringEntities;
 
@@ -16,6 +18,11 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.Storage
         {
         }
 
+        protected override sealed ColumnFamilyFullName GetBosEventLogColumnFamilyFullName()
+        {
+            return new ColumnFamilyFullName(LocalKeyspaceName, bosEventLogColumnFamilyName);
+        }
+
         protected override void ConfigureBusinessObjectSchema(BusinessObjectStoringSchema schema)
         {
             schema.DefineType<MonitoringSearchRequest>(c => c.TypeIdentifier("MonitoringSearchRequest"));
@@ -24,10 +31,15 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.Storage
         protected override void DoConfigureCassandraBasicSchema(CassandraStoringSchema schema)
         {
             schema.ColumnFamily("lock", c => c.Name(ColumnFamilyRepositoryParameters.LockColumnFamily).KeyspaceName(LocalKeyspaceName));
+
+            StrictBosEventLogStorageConfiguration.ConfigureCassandraSchema(schema, LocalKeyspaceName);
+            schema.ColumnFamily(bosEventLogColumnFamilyName, x => x.Name(bosEventLogColumnFamilyName).KeyspaceName(LocalKeyspaceName));
         }
 
         protected override void ConfigureCassandraBusinessObjects(CassandraStoringSchema schema, IContainer container)
         {
         }
+
+        private const string bosEventLogColumnFamilyName = "BosEventLog";
     }
 }
