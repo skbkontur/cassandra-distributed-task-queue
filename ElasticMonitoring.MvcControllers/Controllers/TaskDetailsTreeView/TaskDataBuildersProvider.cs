bcc1a1abb7ea88094d783d88b2aaa88007cf841c
michael <michael@skbkontur.ru>
2016-08-12 16:54:04 +0500

    EDI : Fixed TimeGuid representation in new task monitoring. Task example : https://edi.kontur.ru/AdminTools/Tasks/Details?id=0ae86711-5256-11e6-9c37-66a729d65a16

--- ElasticMonitoring.MvcControllers/Controllers/TaskDetailsTreeView/TaskDataBuildersProvider.cs
+++ ElasticMonitoring.MvcControllers/Controllers/TaskDetailsTreeView/TaskDataBuildersProvider.cs
@@ -1,11 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 
 using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding.Builders;
 using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding.Builders.Base;
 using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding.Builders.Providers;
-using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding.Context;
-using SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.ObjectTreeViewBuilding.Results;
 
 namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.Controllers.TaskDetailsTreeView
 {
@@ -16,27 +13,9 @@ namespace SKBKontur.Catalogue.RemoteTaskQueue.ElasticMonitoring.MvcControllers.C
             yield return new NullValuesBuilder();
             yield return new PrimitiveTypesBuilderWithSpecialValue();
             yield return new ByteArrayBuilder();
+            yield return new TimeGuidBuilder();
             foreach(var builder in base.GetBuilders())
                 yield return builder;
-        }
-    }
-
-    internal class ByteArrayBuilder : BuilderBase
-    {
-        public override IBuildingResult Build(object targetObject, BuildingContext buildingContext)
-        {
-            var taskDataBuildingContext = (buildingContext.ObjectBuildingContext as TaskDataBuildingContext);
-            if(taskDataBuildingContext == null)
-                return NoResult.Instance;
-            if(buildingContext.MemberBuildingContext.DeclaredType == typeof(byte[]))
-            {
-                var urlHelper = taskDataBuildingContext.UrlHelper;
-                var value = targetObject;
-                return RawHtml(string.Format(@"<a href=""{0}""> <span class=""glyphicon glyphicon-download-alt"" aria-hidden=""true""></span> Скачать ({1} байт)</a>",
-                                             urlHelper.Action("GetBytes", new {id = taskDataBuildingContext.TaskId, path = string.Join(".", buildingContext.MemberBuildingContext.PathFromRoot.Select(x => x))}),
-                                             ((byte[])value).Length));
-            }
-            return NoResult.Instance;
         }
     }
 }
