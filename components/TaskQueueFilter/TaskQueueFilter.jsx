8bfbd022b0e375afbea895131259ba0da90d8192
KONTURsivukhin <sivukhin@skbkontur.ru>
2018-05-02 15:50:13 +0500

    Обновил справочную информацию по поиску в мониторинге

--- components/TaskQueueFilter/TaskQueueFilter.jsx
+++ components/TaskQueueFilter/TaskQueueFilter.jsx
@@ -100,26 +100,29 @@ export default class TaskQueueFilter extends React.Component<TaskQueueFilterProp
                     При поиске задач можно пользоваться следующими инструментами поиска:
                     <ol className={cn("modal-list")}>
                         <li>
-                            Ввод значения без дополнительных указаний. В этом случае найдутся все задачи, в полях
-                            которых встречается это значение.
+                            Ввод значения без дополнительных указаний. В этом случае найдутся все задачи, в текстовых
+                            полях которых встречается это значение.
                         </li>
                         <li>
-                            Операторы AND, OR, NOT, скобки между ними. Например:
+                            Операторы AND, OR, NOT, скобки между ними. Например:{" "}
                             <code>(value1 OR value2) AND NOT value3</code>.
                         </li>
                         <li>
                             Ввод значения с указанием конкретного поля, в котором его нужно искать. Поля есть двух
-                            типов: Meta и Data, в полях Meta лежит общая информация по задаче, в полях Data - информация
-                            по типу задачи.<br />
-                            Например: <code>Meta.TaskId:value</code>, <code>Data.ScopeId:value</code>,
-                            <code>Data.Value:(value1 OR value2)</code>.
+                            типов: Meta и Data.SomeTaskName, в полях Meta лежит общая информация по задаче, в полях
+                            Data.SomeTaskName - информация по типу задачи.<br />
+                            Например: <code>Meta.Id:value</code>,{" "}
+                            <code>Data.FtpMessageDeliveryFinished.DocumentCirculationId:value</code>,
+                            <code>Data.\*.DocumentCirculationId:(value1 OR value2)</code>. Конкретный список полей в
+                            Meta можно узнать в RemoteTaskQueue.Monitoring.Storage.RtqElastcisearchSchema
                         </li>
                         <li>
-                            Знаки * и ?. Например, <code>Data.ScopeId:val*</code> или <code>Data.ScopeId:val?e</code>.
-                            Звездочка не может быть в начале искомого значения (<code>Data.ScopeId:*lue</code> не
+                            Знаки * и ?. Например, <code>Meta.Name:val*</code> или <code>Meta.Name:val?e</code>.
+                            Звездочка не может быть в начале искомого значения (<code>Meta.Name:*lue</code> не
                             работает).<br />
                             Знаки можно использовать не только в значениях, но и при указании полей. По запросу{" "}
-                            <code>Data.\*Id:value</code> во всех поля даты, которые заканчиваются на id найдётся value.
+                            <code>Data.DocumentCirculationId.\*Id:value</code> во всех поля даты, которые заканчиваются
+                            на id найдётся value.
                         </li>
                         <li>
                             Задание интервалов для дат. Квадратные скобки при задании интервала означают, что концы
@@ -137,8 +140,8 @@ export default class TaskQueueFilter extends React.Component<TaskQueueFilterProp
                             <code>age:&lt;=10</code>, <code>age:(&gt;=10 AND &lt; 20)</code>.
                         </li>
                         <li>
-                            Проверка полей на заполненность. Например: <code>_missing_:Data.Value</code>
-                            - поле пусто, <code>_exists_:Data.Value</code> - поле заполнено.
+                            Проверка полей на заполненность. Например: <code>NOT _exists_:Meta.ParentTaskId</code>
+                            - поле пусто, <code>_exists_:Meta.ParentTaskId</code> - поле заполнено.
                         </li>
                     </ol>
                 </ModalBody>
