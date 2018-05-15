6c021cb653ed5ed867a8345e5b3a898c4c9b4678
avk <Andrew.Kostousov@gmail.com>
2018-05-15 19:38:03 +0500

    new local cassandra management scripts

--- startAll.SR.bat
+++ startAll.SR.bat
@@ -2,12 +2,10 @@ cd /d %~dp0
 
 %WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Deployment\ResetDatabase.xml /t:Fire 
 
+call ..\MSBuild\DeployTargets\Targets\.start-local-cassandra.cmd
+
 pushd ..\Assemblies\ElasticSearchVNext\Server\bin\
 start elasticsearch.bat
-popd
-
-pushd ..\Assemblies\Cassandra\Server\bin\
-start cassandra.bat
 popd
 
 start "ServiceRunner" "..\Tools.Compiled\ServiceRunner\ServiceRunner.exe" "_StartAllConfigs\startAll.SR.yaml" "-startAllServices"
