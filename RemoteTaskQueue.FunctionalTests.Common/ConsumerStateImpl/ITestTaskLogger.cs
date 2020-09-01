bd121ffe7673cf874f21665f7812e5eec9c76c9c
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2020-09-01 22:24:41 +0500

    Remove func tests for now

--- RemoteTaskQueue.FunctionalTests.Common/ConsumerStateImpl/ITestTaskLogger.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RemoteTaskQueue.FunctionalTests.Common.ConsumerStateImpl
-{
-    public interface ITestTaskLogger
-    {
-        void Log(string loggingTaskIdKey, string taskId);
-        string[] GetAll(string loggingTaskIdKey);
-    }
-}
