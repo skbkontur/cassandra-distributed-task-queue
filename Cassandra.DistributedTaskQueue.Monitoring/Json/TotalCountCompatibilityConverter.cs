c7ad63bc5a912557052de914837ea27655a4f6b6
stasput <stasput@skbkontur.ru>
2024-11-18 16:33:32 +0300

    Fix code style

--- Cassandra.DistributedTaskQueue.Monitoring/Json/TotalCountCompatibilityConverter.cs
+++ Cassandra.DistributedTaskQueue.Monitoring/Json/TotalCountCompatibilityConverter.cs
@@ -10,9 +10,9 @@ internal class TotalCountCompatibilityConverter : JsonConverter<object>
 {
     public override object? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        if (reader.TokenType != JsonTokenType.StartObject) 
+        if (reader.TokenType != JsonTokenType.StartObject)
             return JsonSerializer.Deserialize<object?>(ref reader, options);
-        
+
         var jsonDocument = JsonDocument.ParseValue(ref reader).RootElement;
         return jsonDocument.GetProperty("value").Deserialize<object?>(options);
     }
