92f84c1629f6cafbb08821d9ca81b532e81d848b
Семке Даниил Андреевич <semke@skbkontur.ru>
2025-05-16 10:59:08 +0200

    Update libs & fix pipelines

--- cassandra-distributed-task-queue-ui/patch-version.js
+++ cassandra-distributed-task-queue-ui/patch-version.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const cp = require("child_process");
 
 function execAsync(command, options) {
